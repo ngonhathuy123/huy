ff60c90878618c65bb68accc393a4225fc857dd5
Ngô Nhật Huy <125709687+ngonhathuy123@users.noreply.github.com>
2023-05-09 16:19:03 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,143 @@
+const productSale = [
+    {
+        "type":"rau xà lách",
+        "name":"rau xà lách",
+        "price": "30,000đ/kg" ,
+        "priceSale": "25,000đ/kg",
+        "sale": "-16%",
+        "thumnail": "../img/xa lach.jpg" 
+        },
+    {
+        "type":"Rau muống",
+        "name":"Rau muống",
+        "price": "15,000đ/kg" ,
+        "priceSale": "10,000đ/kg",
+        "sale": "-33%",
+        "thumnail": "../img/rau muong.jpg" 
+        },   
+    {
+        "type":"Rau cải thìa",
+        "name":"Rau cải thìa",
+        "price": "15,000/kg" ,
+        "priceSale": "7,500đ/kg",
+        "sale": "-50%",
+        "thumnail": "../img/rau cai thia.jpg" 
+        },
+    {
+        "type":"Rau cải thơm",
+        "name":"Rau cải thơm",
+        "price": "12,000đ/kg" ,
+        "priceSale": "9,000đ/kg",
+        "sale": "-25%",
+        "thumnail": "../img/rau cai thom.jpg" 
+    }
+]
+const mountainBike=[
+    {
+        "type":"Rau cải cúc ",
+        "name":"Rau cải cúc",
+        "price": "15,000đ/kg" ,
+        "thumnail": "../img/rau cai cuc.jpg" 
+        },
+    {
+        "type":"Rau cải ngọt Phú Yên",
+        "name":"Rau cải ngọt Phú Yên",
+        "price": "12,000đ/kg",
+        "thumnail": "../img/rau cai ngot phu yen.jpg" 
+        } ,   
+    {
+        "type":"Rau cải mầm",
+        "name":"Rau cải mầm",
+        "price": "45,000đ/kg",
+        "thumnail": "../img/rau cai mam.jpg" 
+
+        },
+    {
+        "type":"Bắp cải",
+        "name":"Bắp cải",
+        "price": "15,000đ/kg" ,
+        "thumnail": "../img/bap cai.jpg" 
+
+    }
+]
+const cityBike=[
+    {
+        "type":"Bông cải xanh Úc",
+        "name":"Bông cải xanh Úc",
+        "price": "164,000đ/kg" ,
+        "thumnail": "../img/Bông cải xanh Úc.jpg" 
+        },
+    {
+        "type":"Xà lách búp Mỹ",
+        "name":"Xà lách búp Mỹ",
+        "price": "74,000đ/kg",
+        "thumnail": "../img/Xà lách búp Mỹ.jpg" 
+        } ,   
+    {
+        "type":"Bắp cải tí hon",
+        "name":"Bắp cải tí hon",
+        "price": "50,000đ/kg",
+        "thumnail": "../img/Bắp cải tí hon.jpg" 
+
+        },
+    {
+        "type":"Cải diếp đắng đỏ",
+        "name":"Cải diếp đắng đỏ",
+        "price": "100,000đ/kg" ,
+        "thumnail": "../img/Cải diếp đắng đỏ.jpg" 
+    }
+]
+const racingBike = [
+    {
+        "type":"Rau thơm",
+        "name":"Rau thơm",
+        "price": "30,000đ/kg" ,
+        "thumnail": "../img/rau thơm.jpg" 
+        },
+    {
+        "type":"Rau răm ",
+        "name":"Rau răm",
+        "price": "20,000đ/kg",
+        "thumnail": "../img/Rau răm.jpg" 
+        } ,   
+    {
+        "type":"Hành lá",
+        "name":"Hành lá",
+        "price": "50,000đ/kg",
+        "thumnail": "../img/Hành lá.jpg" 
+        },
+    {
+        "type":"Sả",
+        "name":"Sả",
+        "price": "30,000đ/kg" ,
+        "thumnail": "../img/Sả.jpg" 
+    }
+]
+const childrenBike = [
+    {
+        "type":"Dưa leo",
+        "name":"Dưa leo",
+        "price": "20,000đ/kg" ,
+        "thumnail": "../img/Dưa leo.jpg" 
+        },
+    {
+        "type":"Cà chua",
+        "name":"Cà chua",
+        "price": "25,000đ/kg",
+        "thumnail": "../img/tải xuống.jpg" 
+        } ,   
+    {
+        "type":"Đậu bắp",
+        "name":"Đậu bắp",
+        "price": "35,000đ/kg",
+        "thumnail": "../img/Đậu bắp.jpg" 
+        },
+    {
+        "type":"Cà tím",
+        "name":"Cà tím",
+        "price": "25,000đ/kg" ,
+        "thumnail": "../img/Cà tím.jpg" 
+    }
+]
+const slide = ["../img/cuahang.jpg","../img/cuahang2.jpg" ,"../img/cuahang3.jpg"
+]
